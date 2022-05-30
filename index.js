3f95c8ce5974e34b44f7849ef36ff246da6c75a7
Joffrey <joyjoffrey@gmail.com>
2022-05-30 20:24:30 +0200

    fix function declaration at the top of its call

--- index.js
+++ index.js
@@ -5,31 +5,6 @@ let books =
     : JSON.parse(localStorage.getItem("books"));
 const submitbtn = document.getElementById("submit");
 
-submitbtn.addEventListener("click", function () {
-  const title = document.getElementById("input-title");
-  const author = document.getElementById("input-author");
-  const titleIpnut = title.value;
-  const authorIpnut = author.value;
-  addbook(titleIpnut, authorIpnut);
-  author.value = "";
-  title.value = "";
-});
-
-function addbook(title, author) {
-  if (title.length < 2 && author.length < 1) {
-    const message = document.getElementById("message");
-    message.innerHTML = "please, fill all input fields";
-  } else {
-    const book = {
-      id: books && books.length > 0 ? books[books.length - 1].id + 1 : 1,
-      title: title,
-      author: author,
-    };
-    books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
-    Mybooks();
-  }
-}
 function Mybooks() {
   if (books && books.length) {
     let section = document.querySelector(".books");
@@ -56,9 +31,35 @@ function Mybooks() {
   }
 }
 
+function addbook(title, author) {
+  if (title.length < 2 && author.length < 1) {
+    const message = document.getElementById("message");
+    message.innerHTML = "please, fill all input fields";
+  } else {
+    const book = {
+      id: books && books.length > 0 ? books[books.length - 1].id + 1 : 1,
+      title: title,
+      author: author,
+    };
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    Mybooks();
+  }
+}
+
+submitbtn.addEventListener("click", function () {
+  const title = document.getElementById("input-title");
+  const author = document.getElementById("input-author");
+  const titleIpnut = title.value;
+  const authorIpnut = author.value;
+  addbook(titleIpnut, authorIpnut);
+  author.value = "";
+  title.value = "";
+});
+
 function removeme(id) {
   books = books.filter((book) => book.id != Number(id));
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   Mybooks();
 }
 
