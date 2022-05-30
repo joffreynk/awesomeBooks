1be6f6c28c7342964a00425db8d92b4930709846
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-30 18:09:25 +0000

    Create event listener to add a new book

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let books =
   localStorage.getItem('books') === null
     ? []
     : JSON.parse(localStorage.getItem('books'));
+    const submitbtn = document.getElementById('submit');
+
+    submitbtn.addEventListener('click', function() {
+      const title = document.getElementById('input-title');
+      const author = document.getElementById('input-author');
+      const titleIpnut = title.value;
+      const authorIpnut = author.value;
+      addbook(titleIpnut, authorIpnut);
+      author.value = '';
+      title.value = '';
+    });
